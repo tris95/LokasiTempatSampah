4540cbfc64871193e328de18814af1bb4290bd01
Sutrisno <sutrisno.maret@gmail.com>
2018-11-15 20:17:53 +0700

    tambah

---
+++ app/src/main/java/com/sampah/lokasitempatsampah/activities/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.sampah.lokasitempatsampah.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sampah.lokasitempatsampah.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
