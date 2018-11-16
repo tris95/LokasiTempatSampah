304c669fc3ebb1bfcc2422c9d0993c2bd8edb20f
Sutrisno <sutrisno.maret@gmail.com>
2018-11-16 07:06:08 +0700

    tambah

---
+++ app/src/main/java/com/sampah/lokasitempatsampah/models/ValueAdd.java
@@ -0,0 +1,14 @@
+package com.sampah.lokasitempatsampah.models;
+
+public class ValueAdd {
+    private int success;
+    private String message;
+
+    public int getSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
